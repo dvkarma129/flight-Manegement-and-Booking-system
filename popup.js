e16084fb8f6516654997ef8272577b7bf128b568
dvkarma129 <dvkarma129@gmail.com>
2023-07-13 09:22:58 +0530

    updates

---
+++ popup.js
@@ -0,0 +1,13 @@
+let openpopup = document.getElementById("form")
+let formedit = document.getElementById("xyz")
+
+formedit.addEventListener("click",function open(){
+    alert ("bmnbmnbnb")
+ if (openpopup.style.display==="none"){
+    openpopup.style.display="block"
+ }else{
+    openpopup.style.display==="none"
+ }
+
+});
+open()
